0a20c388246fabd14921270dc14e688a686dcf17
Thuan Nguyen Thai Ninh <thuanntn@nal.vn>
2022-01-09 11:10:15 +0700

    [BE][DMM] Update query to fetch recommended posts

--- hb-dmm/src/main/java/com/ntnt/highblog/dmm/repository/neo4j/PostNodeRepository.java
+++ hb-dmm/src/main/java/com/ntnt/highblog/dmm/repository/neo4j/PostNodeRepository.java
@@ -8,24 +8,23 @@ import java.util.List;
 
 public interface PostNodeRepository extends Neo4jRepository<PostNode, Long> {
     @Query("MATCH (user:User {id: $userId})-[:READ]->(post:Post)-[:HAS_TAG]->(tag:Tag)<-[:HAS_TAG]-(otherPost:Post)"
-        + " WHERE NOT EXISTS ((user)-[:READ]->(otherPost))"
-        + "       AND NOT EXISTS ((user)-[:IS_AUTHOR]->(otherPost))"
+        + " WHERE NOT EXISTS ((user)-[:IS_AUTHOR]->(otherPost))"
         + "       AND NOT EXISTS ((user)-[:IS_AUTHOR]->(post))"
-        + " WITH post, otherPost, tag, COUNT(DISTINCT tag) AS tagIntersection"
+        + " OPTIONAL MATCH (user)-[readOtherPost:READ]->(otherPost)"
+        + " WITH post, otherPost, tag, COUNT(DISTINCT readOtherPost) AS read, COUNT(DISTINCT tag) AS tagIntersection"
         + " MATCH (post)-[:HAS_TAG]->(tagOfPost:Tag)"
-        + " WITH post, otherPost, tag, tagIntersection, COLLECT(DISTINCT tagOfPost.id) AS tagOfPostId"
+        + " WITH post, otherPost, tag, read, tagIntersection, COLLECT(DISTINCT tagOfPost.id) AS tagOfPostId"
         + " MATCH (otherPost)-[:HAS_TAG]->(tagOfOtherPost:Tag)"
-        + " WITH post, otherPost, tag, tagIntersection, tagOfPostId, COLLECT(DISTINCT tagOfOtherPost.id) AS tagOfOtherPostId"
-        + " WITH post, otherPost, tag, tagIntersection, tagOfPostId, tagOfOtherPostId, tagOfPostId+[x IN tagOfOtherPostId WHERE NOT x IN tagOfPostId] AS unionTag "
+        + " WITH post, otherPost, tag, read, tagIntersection, tagOfPostId, COLLECT(DISTINCT tagOfOtherPost.id) AS tagOfOtherPostId"
+        + " WITH post, otherPost, tag, read, tagIntersection, tagOfPostId, tagOfOtherPostId, tagOfPostId+[x IN tagOfOtherPostId WHERE NOT x IN tagOfPostId] AS unionTag "
         + " WITH otherPost, ((1.0*tagIntersection)/SIZE(unionTag)) AS jaccard"
-        + " ORDER BY jaccard DESC, otherPost.numberOfVotes DESC, otherPost.numberOfFavorites DESC, otherPost.title DESC, id(otherPost) ASC"
+        + " ORDER BY read ASC, jaccard DESC, otherPost.numberOfVotes DESC, otherPost.numberOfFavorites DESC, otherPost.title DESC, id(otherPost) ASC"
         + " RETURN DISTINCT otherPost"
         + " SKIP $skip LIMIT $limit")
     List<PostNode> fetchRecommendPostsByUserId(Long userId, Long skip, Integer limit);
 
     @Query("MATCH (user:User {id: $userId})-[:READ]->(post:Post)-[:HAS_TAG]->(tag:Tag)<-[:HAS_TAG]-(otherPost:Post)"
-        + " WHERE NOT EXISTS ((user)-[:READ]->(otherPost))"
-        + "       AND NOT EXISTS ((user)-[:IS_AUTHOR]->(otherPost))"
+        + " WHERE NOT EXISTS ((user)-[:IS_AUTHOR]->(otherPost))"
         + "       AND NOT EXISTS ((user)-[:IS_AUTHOR]->(post))"
         + " WITH post, otherPost, tag, COUNT(DISTINCT tag) AS tagIntersection"
         + " MATCH (post)-[:HAS_TAG]->(tagOfPost:Tag)"
